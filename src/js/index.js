04346054c7bb9a731fe1ec8e9f47ad89d567f1cc
Omer Ahmed <omera407@gmail.com>
2022-06-10 20:24:39 -0600

    f

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
-console.log("log");
+// import {ref as dataRef, get, set, update} from 'firebase/database';
+// import ref from "firebase/database";
+import { get } from "firebase/database";
+import { db, storage } from "./libs/firebase/firebaseConfig.js";
+// ^ you can take the '.js' off the end of the import line once you fully commit to using a module bundler to test + deploy
+// since we're still using Live Server to test, we're not quite there yet!
+// import { db, storage } from "./libs/firebase/firebaseConfig";
+
+console.log("logg");
+console.log(db);
+console.log(storage);
+
+async function pageInit() {
+  const plushieRef = ref(db, "plushies/");
+  const plushieSnapshot = await get(plushieRef);
+  const data = plushieSnapshot.val();
+
+  console.log(plushieRef);
+  console.log(plushieSnapshot);
+  console.log(data);
+  console.log(Object.values(data));
+}
+
+pageInit();
--- src/js/libs/firebase/firebaseConfig.js
+++ src/js/libs/firebase/firebaseConfig.js
@@ -3,15 +3,29 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
+// TEMPORARY imports for testing project without using a module bundler
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+// import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-database.js";
+// import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-storage.js";
+
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.STOREFRONT_FIREBASE_API_KEY,
+//   authDomain: process.env.STOREFRONT_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.STOREFRONT_FIREBASE_DATABASE_URL,
+//   projectId: process.env.STOREFRONT_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.STOREFRONT_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.STOREFRONT_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.STOREFRONT_FIREBASE_APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.STOREFRONT_FIREBASE_API_KEY,
-  authDomain: process.env.STOREFRONT_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.STOREFRONT_FIREBASE_DATABASE_URL,
-  projectId: process.env.STOREFRONT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.STOREFRONT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.STOREFRONT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.STOREFRONT_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAjj63sodNxmevObtdJSHoul4WbohicRCg",
+  authDomain: "storefront-e0b92.firebaseapp.com",
+  databaseURL: "https://storefront-e0b92-default-rtdb.firebaseio.com",
+  projectId: "storefront-e0b92",
+  storageBucket: "storefront-e0b92.appspot.com",
+  messagingSenderId: "27134202615",
+  appId: "1:27134202615:web:1c96bca0fa9bee378979f2",
 };
 
 // Initialize Firebase Services
